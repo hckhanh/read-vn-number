8729e3f8357fd278e7722cf64fa575c13b32238e
Khanh Hoang <hoangchaukhanh93@gmail.com>
2018-10-19 10:41:18 +0700

    Fix constructors duplication

--- src/Billion.ts
+++ src/Billion.ts
@@ -4,10 +4,6 @@ import Numbers from './Numbers'
  * A group three numbers, a component in billion position of the input number
  */
 export default class Billion extends Numbers {
-  constructor(s: string) {
-    super(s)
-  }
-
   public read(firstNumber?: boolean, beforeBillion?: boolean): string {
     if (this.first !== '0' || this.second !== '0' || this.last !== '0') {
       return `${super.read(firstNumber)} tỉ`
--- src/Million.ts
+++ src/Million.ts
@@ -4,10 +4,6 @@ import Numbers from './Numbers'
  * A group three numbers, a component in million position of the input number
  */
 export default class MillionNumber extends Numbers {
-  constructor(s: string) {
-    super(s)
-  }
-
   public read(firstNumber?: boolean, beforeBillion?: boolean): string {
     if (this.first !== '0' || this.second !== '0' || this.last !== '0') {
       return `${super.read(firstNumber)} triệu`
