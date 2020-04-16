4599df3d4e8b26f85e0a5dd6c7314c707ba6741b
Khánh Hoàng <hoangchaukhanh93@gmail.com>
2020-04-16 09:20:40 +0700

    fix typos and grammar mistakes
    Signed-off-by: Khánh Hoàng <hoangchaukhanh93@gmail.com>

--- src/Thousand.ts
+++ src/Thousand.ts
@@ -1,7 +1,7 @@
 import Zerofill from './Zerofill'
 
 /**
- * A group three numbers, a component in thousand position of the input number
+ * A group three numbers, a component in a thousand position of the input number
  */
 export default class Thousand extends Zerofill {
   protected get unitName(): string {
