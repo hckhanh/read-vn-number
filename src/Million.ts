3f23be312299757d19c489c3c3de162a04c8f3e4
Khánh Hoàng <hoangchaukhanh93@gmail.com>
2020-04-16 09:20:40 +0700

    fix typos and grammar mistakes
    Signed-off-by: Khánh Hoàng <hoangchaukhanh93@gmail.com>

--- src/Million.ts
+++ src/Million.ts
@@ -1,7 +1,7 @@
 import Zerofill from './Zerofill'
 
 /**
- * A group three numbers, a component in million position of the input number
+ * A group three numbers, a component in a million position of the input number
  */
 export default class MillionNumber extends Zerofill {
   protected get unitName(): string {
