eb372dafeeb4f6d77945b659c9c00f8530e3c257
Khánh Hoàng <hoangchaukhanh93@gmail.com>
2020-08-26 20:17:48 +0700

    chore(prettier): format un-formatted files
    Signed-off-by: Khánh Hoàng <hoangchaukhanh93@gmail.com>

--- tests/classes/Number.test.ts
+++ tests/classes/Number.test.ts
@@ -1,152 +1,152 @@
 import { expect } from 'chai'
 import Numbers from '../../src/Numbers'
 
-describe('Numbers', function() {
-  it('should read number: 5', function() {
+describe('Numbers', function () {
+  it('should read number: 5', function () {
     const s: string = '5'
     const number = new Numbers(s)
     expect(number.read()).to.equal('năm')
   })
 
-  it('should read number: 32', function() {
+  it('should read number: 32', function () {
     const s: string = '32'
     const number = new Numbers(s)
     expect(number.read()).to.equal('ba mươi hai')
   })
 
-  it('should read number: 15', function() {
+  it('should read number: 15', function () {
     const s: string = '15'
     const number = new Numbers(s)
     expect(number.read()).to.equal('mười lăm')
   })
 
-  it('should read number: 21', function() {
+  it('should read number: 21', function () {
     const s: string = '21'
     const number = new Numbers(s)
     expect(number.read()).to.equal('hai mươi mốt')
   })
 
-  it('should read number: 001', function() {
+  it('should read number: 001', function () {
     const s: string = '001'
     const number = new Numbers(s)
     expect(number.read()).to.equal('không trăm lẻ một')
   })
 
-  it('should read number: 068', function() {
+  it('should read number: 068', function () {
     const s: string = '068'
     const number = new Numbers(s)
     expect(number.read()).to.equal('không trăm sáu mươi tám')
   })
 
-  it('should read number: 060', function() {
+  it('should read number: 060', function () {
     const s: string = '060'
     const number = new Numbers(s)
     expect(number.read()).to.equal('không trăm sáu mươi')
   })
 
-  it('should read number: 162', function() {
+  it('should read number: 162', function () {
     const s: string = '162'
     const number = new Numbers(s)
     expect(number.read()).to.equal('một trăm sáu mươi hai')
   })
 
-  it('should read number: 601', function() {
+  it('should read number: 601', function () {
     const s: string = '601'
     const number = new Numbers(s)
     expect(number.read()).to.equal('sáu trăm lẻ một')
   })
 
-  it('should read number: 611', function() {
+  it('should read number: 611', function () {
     const s: string = '611'
     const number = new Numbers(s)
     expect(number.read()).to.equal('sáu trăm mười một')
   })
 
-  it('should read number: 221', function() {
+  it('should read number: 221', function () {
     const s: string = '221'
     const number = new Numbers(s)
     expect(number.read()).to.equal('hai trăm hai mươi mốt')
   })
 
-  it('should read number: 155', function() {
+  it('should read number: 155', function () {
     const s: string = '155'
     const number = new Numbers(s)
     expect(number.read()).to.equal('một trăm năm mươi lăm')
   })
 
-  it('should read number: 600', function() {
+  it('should read number: 600', function () {
     const s: string = '600'
     const number = new Numbers(s)
     expect(number.read()).to.equal('sáu trăm')
   })
 
-  it('should read number: 610', function() {
+  it('should read number: 610', function () {
     const s: string = '610'
     const number = new Numbers(s)
     expect(number.read()).to.equal('sáu trăm mười')
   })
 
-  it('should read first number: 1', function() {
+  it('should read first number: 1', function () {
     const s: string = '1'
     const number = new Numbers(s)
     expect(number.read(true)).to.equal('một')
   })
 
-  it('should read first number: 10', function() {
+  it('should read first number: 10', function () {
     const s: string = '10'
     const number = new Numbers(s)
     expect(number.read(true)).to.equal('mười')
   })
 
-  it('should read first number: 21', function() {
+  it('should read first number: 21', function () {
     const s: string = '21'
     const number = new Numbers(s)
     expect(number.read(true)).to.equal('hai mươi mốt')
   })
 
-  it('should read first number: 221', function() {
+  it('should read first number: 221', function () {
     const s: string = '221'
     const number = new Numbers(s)
     expect(number.read(true)).to.equal('hai trăm hai mươi mốt')
   })
 
-  it('should read number: 000', function() {
+  it('should read number: 000', function () {
     const s: string = '000'
     const number = new Numbers(s)
     expect(number.read()).to.equal('')
   })
 
-  it('should read number: 000', function() {
+  it('should read number: 000', function () {
     const s: string = '000'
     const number = new Numbers(s)
     expect(number.read(true)).to.equal('không')
   })
 
-  it('should read number: 601', function() {
+  it('should read number: 601', function () {
     const s: string = '601'
     const number = new Numbers(s)
     expect(number.read(true)).to.equal('sáu trăm lẻ một')
   })
 
-  it('should read first number: 1, before billion group', function() {
+  it('should read first number: 1, before billion group', function () {
     const s: string = '1'
     const number = new Numbers(s)
     expect(number.read(true, true)).to.equal('một nghìn')
   })
 
-  it('should read first number: 1, before billion group', function() {
+  it('should read first number: 1, before billion group', function () {
     const s: string = '1'
     const number = new Numbers(s)
     expect(number.read(true, false)).to.equal('một')
   })
 
-  it('should read first number: 1, before billion group', function() {
+  it('should read first number: 1, before billion group', function () {
     const s: string = '001'
     const number = new Numbers(s)
     expect(number.read(false, true)).to.equal('không trăm lẻ một')
   })
 
-  it('should read number: 000, before billion group', function() {
+  it('should read number: 000, before billion group', function () {
     const s: string = '000'
     const number = new Numbers(s)
     expect(number.read(false, true)).to.equal('')
--- tests/classes/NumberReader.test.ts
+++ tests/classes/NumberReader.test.ts
@@ -1,120 +1,130 @@
 import { expect } from 'chai'
 import NumberReader from '../../src/NumberReader'
 
-describe('NumberReader', function() {
-  it('should throw error when read number: a.000', function() {
+describe('NumberReader', function () {
+  it('should throw error when read number: a.000', function () {
     const number: string = 'a000'
-    expect(()=>NumberReader.read(number)).to.throw('Invalid number: a')
+    expect(() => NumberReader.read(number)).to.throw('Invalid number: a')
   })
 
-  it('should read number: 1.000', function() {
+  it('should read number: 1.000', function () {
     const number: number = 1000
     expect(NumberReader.read(number)).to.equal('một nghìn')
   })
 
-  it('should read number: 60.000', function() {
+  it('should read number: 60.000', function () {
     const number: number = 60000
     expect(NumberReader.read(number)).to.equal('sáu mươi nghìn')
   })
 
-  it('should read number: 60', function() {
+  it('should read number: 60', function () {
     const number: number = 60
     expect(NumberReader.read(number)).to.equal('sáu mươi')
   })
 
-  it('should read number: 600.000', function() {
+  it('should read number: 600.000', function () {
     const number: number = 600000
     expect(NumberReader.read(number)).to.equal('sáu trăm nghìn')
   })
 
-  it('should read number: 600.001', function() {
+  it('should read number: 600.001', function () {
     const number: number = 600001
-    expect(NumberReader.read(number)).to.equal('sáu trăm nghìn không trăm lẻ một')
+    expect(NumberReader.read(number)).to.equal(
+      'sáu trăm nghìn không trăm lẻ một'
+    )
   })
 
-  it('should read number: 1.060.000', function() {
+  it('should read number: 1.060.000', function () {
     const number: number = 1060000
     expect(NumberReader.read(number)).to.equal(
       'một triệu không trăm sáu mươi nghìn'
     )
   })
 
-  it('should read number: 15.000.000', function() {
+  it('should read number: 15.000.000', function () {
     const number: number = 15000000
     expect(NumberReader.read(number)).to.equal('mười lăm triệu')
   })
 
-  it('should read number: 19.900.000', function() {
+  it('should read number: 19.900.000', function () {
     const number: number = 19900000
-    expect(NumberReader.read(number)).to.equal('mười chín triệu chín trăm nghìn')
+    expect(NumberReader.read(number)).to.equal(
+      'mười chín triệu chín trăm nghìn'
+    )
   })
 
-  it('should read number: 30.000.000', function() {
+  it('should read number: 30.000.000', function () {
     const number: number = 30000000
     expect(NumberReader.read(number)).to.equal('ba mươi triệu')
   })
 
-  it('should read number: 60.000.000', function() {
+  it('should read number: 60.000.000', function () {
     const number: number = 60000000
     expect(NumberReader.read(number)).to.equal('sáu mươi triệu')
   })
 
-  it('should read number: 100.000.000', function() {
+  it('should read number: 100.000.000', function () {
     const number: number = 100000000
     expect(NumberReader.read(number)).to.equal('một trăm triệu')
   })
 
-  it('should read number: 500.000.000', function() {
+  it('should read number: 500.000.000', function () {
     const number: number = 500000000
     expect(NumberReader.read(number)).to.equal('năm trăm triệu')
   })
 
-  it('should read number: 1.000.000.000', function() {
+  it('should read number: 1.000.000.000', function () {
     const number: number = 1000000000
     expect(NumberReader.read(number)).to.equal('một tỷ')
   })
 
-  it('should read number: 1.100.000.000', function() {
+  it('should read number: 1.100.000.000', function () {
     const number: number = 1100000000
     expect(NumberReader.read(number)).to.equal('một tỷ một trăm triệu')
   })
 
-  it('should read number: 2.000.000.000', function() {
+  it('should read number: 2.000.000.000', function () {
     const number: number = 2000000000
     expect(NumberReader.read(number)).to.equal('hai tỷ')
   })
 
-  it('should read number: 1.000.000.000.000', function() {
+  it('should read number: 1.000.000.000.000', function () {
     const number: string = '1000000000000'
     expect(NumberReader.read(number)).to.equal('một nghìn tỷ')
   })
 
-  it('should read number: 1.001.000.000.000', function() {
+  it('should read number: 1.001.000.000.000', function () {
     const number: string = '1001000000000'
     expect(NumberReader.read(number)).to.equal('một nghìn không trăm lẻ một tỷ')
   })
 
-  it('should read number: 1.001.500.000.000', function() {
+  it('should read number: 1.001.500.000.000', function () {
     const number: string = '1001500000000'
-    expect(NumberReader.read(number)).to.equal('một nghìn không trăm lẻ một tỷ năm trăm triệu')
+    expect(NumberReader.read(number)).to.equal(
+      'một nghìn không trăm lẻ một tỷ năm trăm triệu'
+    )
   })
 
-  it('should read number: 1.001.500.100.000', function() {
+  it('should read number: 1.001.500.100.000', function () {
     const number: string = '1001500100000'
-    expect(NumberReader.read(number)).to.equal('một nghìn không trăm lẻ một tỷ năm trăm triệu một trăm nghìn')
+    expect(NumberReader.read(number)).to.equal(
+      'một nghìn không trăm lẻ một tỷ năm trăm triệu một trăm nghìn'
+    )
   })
 
-  it('should read number: 1.001.500.100.001', function() {
+  it('should read number: 1.001.500.100.001', function () {
     const number: string = '1001500100001'
-    expect(NumberReader.read(number)).to.equal('một nghìn không trăm lẻ một tỷ năm trăm triệu một trăm nghìn không trăm lẻ một')
+    expect(NumberReader.read(number)).to.equal(
+      'một nghìn không trăm lẻ một tỷ năm trăm triệu một trăm nghìn không trăm lẻ một'
+    )
   })
 
-  it('should read number: 1.000.001.000.000.000', function() {
+  it('should read number: 1.000.001.000.000.000', function () {
     const number: string = '1000001000000000'
     expect(NumberReader.read(number)).to.equal('một triệu không trăm lẻ một tỷ')
   })
 
-  it('should read number: 1.000.000.000.000.000.000', function() {
+  it('should read number: 1.000.000.000.000.000.000', function () {
     const number: string = '1000000000000000000'
     expect(NumberReader.read(number)).to.equal('một tỷ tỷ')
   })
