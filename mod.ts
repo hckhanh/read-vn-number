efb183124768107c779c58c0b769bec1c99d6ca3
Khánh Hoàng <hi@khanh.sh>
2024-02-26 09:42:31 +0700

    chore(build): Add deno.json and mod.ts, update the build workflows and config files
    This commit includes the addition of deno.json and mod.ts files to better structure the project and also enhances the build processes by implementing an updated jsr-publish workflow in GitHub. This update ensures the version in deno.json is updated during the build. Additionally, changes have been made to tsconfig.json file to include 'mod.ts' and 'renovate.json' file to enhance versioning practices.

---
+++ mod.ts
@@ -0,0 +1,14 @@
+/**
+ * This is a helper that convert a number to a string like the way a real Vietnamese read it.
+ *
+ * @example
+ * ```ts
+ * import readNumber from 'read-vn-number'
+ *
+ * readNumber('19990000') // or readNumber(19990000)
+ * // output: mười chín triệu chín trăm chín mươi nghìn
+ * ```
+ *
+ * @module
+ */
+export * from './src'
